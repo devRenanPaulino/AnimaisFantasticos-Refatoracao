dab814ebeb9ef1e8712585a58c52213f9bde3269
Renan Paulino <dev.renanpaulino@gmail.com>
2026-01-17 23:33:42 -0300

    chore: Adjusting the position of the anima scroll to the end.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scrool-suave.js";
-import initAnimacaoScroll from "./modules/scrool-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
@@ -9,11 +8,11 @@ import initMenuMobile from "./modules/menuMobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetchAnimais.js";
 import initFetchBitcoin from "./modules/fetchBitcoin.js";
+import initAnimacaoScroll from "./modules/scrool-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -23,3 +22,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
