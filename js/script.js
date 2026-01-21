d83a9e1cb2aeeb546384ffa139dee0358b6826d8
Renan Paulino <dev.renanpaulino@gmail.com>
2026-01-21 19:51:43 -0300

    refactor: transformed dropdownMenu into class

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown.js";
+import DropdownMenu from "./modules/dropdown.js";
 import initMenuMobile from "./modules/menuMobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetchAnimais.js";
@@ -28,7 +28,9 @@ tooltip.init();
 const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 scrollAnima.init();
 
-initDropdownMenu();
+const dropDownMenus = new DropdownMenu("[data-dropdown]");
+dropDownMenus.init();
+
 initMenuMobile();
 initFuncionamento();
 
