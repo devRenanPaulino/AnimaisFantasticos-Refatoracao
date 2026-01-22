f067a63636d655d36d621477c9d3d95be0b13e6d
Renan Paulino <dev.renanpaulino@gmail.com>
2026-01-22 11:47:55 -0300

    refactor: transformed funcionamento into class

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown.js";
 import MenuMobile from "./modules/menuMobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetchAnimais.js";
 import fetchBitcoin from "./modules/fetchBitcoin.js";
 import ScrollAnima from "./modules/scrollAnima.js";
@@ -34,8 +34,8 @@ dropDownMenus.init();
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
-
-initFuncionamento();
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
 
 fetchAnimais('../animaisapi.json', ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
